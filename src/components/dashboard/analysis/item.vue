d1e5fc4efc2c419958d108ff3170d4438f609f84
jyrncl <2781339212@qq.com>
2022-09-13 10:29:19 +0800

    去除eslint

--- src/components/dashboard/analysis/item.vue
+++ src/components/dashboard/analysis/item.vue
@@ -1,16 +1,16 @@
 <template>
-<div class="analysis-item-wrapper">
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-  </el-tabs>
-</div>
+  <div class="analysis-item-wrapper">
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="User" name="first">User</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
-import type {TabsPaneContext} from 'element-plus';
+import { ref } from "vue";
+import type { TabsPaneContext } from "element-plus";
 
-const activeName = ref('');
+const activeName = ref("");
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
   console.log(tab, event);
