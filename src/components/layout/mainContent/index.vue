7bdb4bea5bffd384c40f124a700c169215a4fb07
jyrnc <2781339212@qq.com>
2022-11-02 19:40:51 +0800

    router-view

--- src/components/layout/mainContent/index.vue
+++ src/components/layout/mainContent/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="mainContent">
     <router-view v-slot="{ Component }">
-      <keep-alive-wrapper :component="Component" />
+      <template v-if="route.meta['keepAlive']">
+        <keep-alive>
+          <transition :name="`${$prefix}-${commonStore.transitionName}`">
+            <component :is="Component"/>
+          </transition>
+        </keep-alive>
+      </template>
+      <template v-else>
+        <transition :name="`${$prefix}-${commonStore.transitionName}`">
+          <component :is="Component"/>
+        </transition>
+      </template>
     </router-view>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRoute } from "vue-router"
+import { useCommonStore } from "@/store"
+const commonStore = useCommonStore();
+const route = useRoute();
 
 </script>
 
